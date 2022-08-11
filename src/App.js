c5ba3c229a7ddcbd5bda4ea03def80a7c0fdad7a
Emily <emilydta@users.noreply.github.com>
2022-08-12 00:17:45 +1000

    add basic dropzone functionality

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import { useState } from 'react';
 
 function App() {
 
-  const [audioFile, setAudioFile] = useState();
+  const [audioFile, setAudioFile] = useState('');
+
+  function dropEvent(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    let files = e.dataTransfer.files
+    setAudioFile(files[0].type.includes("audio") ? files[0] : audioFile)
+  }
 
   return (
     <div className="App">
       <AudioForm
         handleChange={(e) => setAudioFile(e.target.files[0])}
         audioFile={audioFile}
-      />  
+        dropEvent={(e) => dropEvent(e)}
+      />
     </div>
   );
 }
--- src/components/audioForm.js
+++ src/components/audioForm.js
@@ -1,13 +1,31 @@
 function AudioForm(props) {
+
+    function preventDefaults(e) {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
     return (
         <form className="audio-form">
-            <label>Upload Audio File</label>
-            <input type='file' accept="audio/*" placeholder="" className="audio-input" onChange={props.handleChange}></input>
-            <audio
-                controls
-                src={props.audioFile ? URL.createObjectURL(props.audioFile) : null}
+            <div id="dropzone"
+                onDragEnter={(e) => preventDefaults(e)}
+                onDragOver={(e) => preventDefaults(e)}
+                onDragLeave={(e) => preventDefaults(e)}
+                onDrop={props.dropEvent}
             >
-            </audio>
+                <label>Upload Audio File</label>
+                <input type='file'
+                    style={{ color: "transparent" }}
+                    title=" "
+                    accept="audio/*"
+                    className="audio-input"
+                    onChange={props.handleChange}></input>
+                <audio
+                    controls
+                    src={props.audioFile ? URL.createObjectURL(props.audioFile) : null}
+                >
+                </audio>
+            </div>
             <button type="submit" onSubmit={props.submit}>Next</button>
         </form>
     )
